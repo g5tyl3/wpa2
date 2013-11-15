bc410d4f3a3098a5e38480e031891111e340408a
Jan Stepanek <jan.stepanek@nodus.cz>
2013-11-15 17:46:06 +0100

    new entity User added

---
+++ src/main/java/cz/cvut/stepaj29/springHello/bo/User.java
@@ -0,0 +1,40 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package cz.cvut.stepaj29.springHello.bo;
+
+import java.io.Serializable;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+/**
+ *
+ * @author jan
+ */
+@Entity
+@Table(name="users")
+public class User implements Serializable{
+    @Id
+    private Long id;
+    protected String name;
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+}
