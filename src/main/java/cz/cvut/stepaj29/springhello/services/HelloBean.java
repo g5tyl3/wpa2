1b210cebb3ef97f09f72579203b318526d08f0f7
Jan Stepanek <jan.stepanek@nodus.cz>
2013-11-15 16:16:03 +0100

    bean in form

--- src/main/java/cz/cvut/stepaj29/springhello/services/HelloBean.java
+++ src/main/java/cz/cvut/stepaj29/springhello/services/HelloBean.java
@@ -6,6 +6,9 @@
 
 package cz.cvut.stepaj29.springhello.services;
 
+import javax.faces.application.FacesMessage;
+import javax.faces.context.FacesContext;
+import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Component;
 
 /**
@@ -13,10 +16,25 @@ import org.springframework.stereotype.Component;
  * @author jan
  */
 @Component
+@Scope("request")
 public class HelloBean {
+    
+    protected String userName;
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
     public String sayHello()
     {
         return "hello world!";
     }
-    
+    public void execute()
+    {
+        FacesMessage message = new FacesMessage("ok", "Name " + userName + " submitted");
+        FacesContext.getCurrentInstance().addMessage(null, message);
+    }
 }
